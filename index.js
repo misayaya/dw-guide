2b2c017ea93738caa17c9c1c3deee0fd1fda75aa
misayaya <31941937+misayaya@users.noreply.github.com>
2017-09-13 21:04:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ module.exports = function DWGuide(dispatch) {
 		if (!enabled) return;
 		
 		if (event.huntingZoneId == dw) {
-			if(event.templateId == bandersnatch) {
+			
 				boss = event;
 			}
 		}
